deec24772031f85d2b60fbe531f94ca86d3dc843
sharifu majid <sharifumajid3@gmail.com>
2024-10-18 06:16:03 +0300

    base setup

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import pendulum
-from django.db.models import User
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
